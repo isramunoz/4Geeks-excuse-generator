28344219642914c34c01e944b6de7e245481472a
Santiago de Aguirre <santiago.deaguirre@gmail.com>
2022-03-03 13:00:19 -0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'My dragon', 'My girlfriend', 'My mentor'];
+  let action = ['ate', 'peed', 'burnt', 'crushed', 'broke'];
+  let what = ['my homework', 'my bedroom', 'my lunch', 'my keys', 'the car', 'my wallet']
+  let when = ['before the date', 'right on time', 'before the class', 'before I left'];
+  let whoRandom = who[Math.floor(Math.random() * who.length)];
+  console.log(whoRandom)
+  let actionRandom = action[Math.floor(Math.random() * action.length)];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+  document.getElementById('excuse').textContent = whoRandom + ' ' + actionRandom + ' ' + whatRandom + ' ' + whenRandom;
+  
 };
+console.log('Hello World')
