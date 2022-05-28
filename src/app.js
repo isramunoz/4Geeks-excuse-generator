ed6c06ed50e8fe023d3542848ff5343a10413744
Israel Munoz <imunoz.developer@gmail.com>
2022-05-28 17:10:24 -0400

    Se rreglan algunos errores de funcionalidad en js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 
 window.onload = function() {
   //write your code here
-  let who = ['The dog', 'My grandma', 'My dragon', 'My girlfriend', 'My mentor'];
+  let who = ['The cat', 'My grandma', 'My dragon', 'My girlfriend', 'My mentor'];
   let action = ['ate', 'peed', 'burnt', 'crushed', 'broke'];
   let what = ['my homework', 'my bedroom', 'my lunch', 'my keys', 'the car', 'my wallet']
   let when = ['before the date', 'right on time', 'before the class', 'before I left'];
-  let whoRandom = who[Math.floor(Math.random() * who.length)];
+  let whoRandom = who[Math.floor(Math.random() * who.length)]; 
   console.log(whoRandom)
   let actionRandom = action[Math.floor(Math.random() * action.length)];
   let whatRandom = what[Math.floor(Math.random() * what.length)];
